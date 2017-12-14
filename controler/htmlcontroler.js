ce67e4e478f8cdc0647391151e64550aef7d89cf
EugeneTooms <eugenevictortooms8@gmail.com>
2017-12-14 15:35:05 +0100

    Zakomentirao htmlcontroler stare API-e, neka ostanu da budu vidljive za budučnost

--- controler/htmlcontroler.js
+++ controler/htmlcontroler.js
@@ -7,77 +7,77 @@ var config = require('../config');
 var kon = config.getDBconnection();
 
 
-router.get('/articles', function(req, res, next){
-    kon.query('SELECT * from articles',
-        function(error, results){
-                if(error) {
-                    return res.status(500).json({
-                        title: 'An error has occured',
-                        error : error
-                    });
-                }			
-                res.status(200).json({
-                    message: 'Success',
-                    obj: results
-                });
-        }
-    );
-});
-router.get('/receivingitems/:id', function(req, res, next){
+// router.get('/articles', function(req, res, next){
+//     kon.query('SELECT * from articles',
+//         function(error, results){
+//                 if(error) {
+//                     return res.status(500).json({
+//                         title: 'An error has occured',
+//                         error : error
+//                     });
+//                 }			
+//                 res.status(200).json({
+//                     message: 'Success',
+//                     obj: results
+//                 });
+//         }
+//     );
+// });
+// router.get('/receivingitems/:id', function(req, res, next){
     
-    kon.query('SELECT * from receiving_items WHERE receiving_id = ' + req.params.id,
-        function(error, results){
-                if(error) {
-                    return res.status(500).json({
-                        title: 'An error has occured',
-                        error : error
-                    });
-                }			
-                res.status(200).json({
-                    message: 'Success',
-                    obj: results
-                });
-        }
-    );
-});
-router.get('/receivings_list', function(req, res, next){
+//     kon.query('SELECT * from receiving_items WHERE receiving_id = ' + req.params.id,
+//         function(error, results){
+//                 if(error) {
+//                     return res.status(500).json({
+//                         title: 'An error has occured',
+//                         error : error
+//                     });
+//                 }			
+//                 res.status(200).json({
+//                     message: 'Success',
+//                     obj: results
+//                 });
+//         }
+//     );
+// });
+// router.get('/receivings_list', function(req, res, next){
     
-    kon.query('SELECT * from receivings',
-        function(error, results){
-                if(error) {
-                    return res.status(500).json({
-                        title: 'An error has occured',
-                        error : error
-                    });
-                }			
-                res.status(200).json({
-                    message: 'Success',
-                    obj: results
-                });
-        }
-    );
-});
+//     kon.query('SELECT * from receivings',
+//         function(error, results){
+//                 if(error) {
+//                     return res.status(500).json({
+//                         title: 'An error has occured',
+//                         error : error
+//                     });
+//                 }			
+//                 res.status(200).json({
+//                     message: 'Success',
+//                     obj: results
+//                 });
+//         }
+//     );
+// });
 
-router.post('/article', function(req, res, next){
-    console.log(req.body);
-    res.status(200).json({
-        message : 'Success',
-        obj : req.body.content
-    });
-    // kon.query('INSERT INTO articles SET ?', req.body,
-    //     function(error, results){
-    //             if(error) {
-    //                 return res.status(500).json({
-    //                     title: 'An error has occured',
-    //                     error : error
-    //                 });
-    //             }			
-    //             res.status(200).json({
-    //                 message: 'Success',
-    //                 obj: results
-    //             });
-    //     }
-    // );
-});
+// router.post('/article', function(req, res, next){
+//     console.log(req.body);
+//     res.status(200).json({
+//         message : 'Success',
+//         obj : req.body.content
+//     });
+//     // kon.query('INSERT INTO articles SET ?', req.body,
+//     //     function(error, results){
+//     //             if(error) {
+//     //                 return res.status(500).json({
+//     //                     title: 'An error has occured',
+//     //                     error : error
+//     //                 });
+//     //             }			
+//     //             res.status(200).json({
+//     //                 message: 'Success',
+//     //                 obj: results
+//     //             });
+//     //     }
+//     // );
+// });
 
 module.exports = router;
