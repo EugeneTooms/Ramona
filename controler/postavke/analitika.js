5ca8e286c3c0596ed8d9c95c146b14eac00ff029
EugeneTooms <eugenevictortooms8@gmail.com>
2018-04-28 12:44:41 +0200

    otpisi

--- controler/postavke/analitika.js
+++ controler/postavke/analitika.js
@@ -83,121 +83,151 @@ art_show_gr.naziv;
 
 
     */
+   kon.query('truncate bot_inventura_detail',
+   function(error, results){
+           if(error) {
+               return res.status(500).json({
+                   title: 'An error has occured',
+                   error : error
+               });
+           }   kon.query(`INSERT INTO bot_inventura_detail (inventory_id, location_id, article_id, kolicina)
+                SELECT 
+                    id, 
+                    1, 
+                    product_id,
+                    sum(on_hand)
+                FROM bot_partender_import
+                where product_id is not null
+                group by 
+                    id, 
+                    product_id`,
+           function(error, results){
+                   if(error) {
+                       return res.status(500).json({
+                           title: 'An error has occured',
+                           error : error
+                       });
+                   }    kon.query(`
+                   select 
+                   article_id,
+                   articles.name, 
+                   art_show_gr.naziv as grupa,
+               
+                   sum(prije) as prije,
+                   sum(ulazi) as ulazi,
+                   sum(prodaja) as prodaja,
+                   sum(otpisi) as otpisi,
+                   sum(poslije) as poslije
+               from
+               (
+                   (select 
+                       articles.id as article_id, 
+                       sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as prije,
+                       0 as ulazi,
+                       0 as prodaja,
+                       0 as otpisi,
+                       sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as poslije
+                       from 
+                           articles
+                       LEFT JOIN 
+                           bot_inventura_detail on articles.id = bot_inventura_detail.article_id
+                       LEFT JOIN 
+                           bot_inventura_master on bot_inventura_detail.location_id = bot_inventura_master.location_id and
+                           bot_inventura_detail.inventory_id = bot_inventura_master.inventory_id
+                       where 
+                           bot_inventura_master.inventory_id >= ?
+                       group by
+                           articles.id
+                   )
+                   union all
+                   (select 
+                       receiving_items.article_id,
+                       0 as prije,
+                       sum(cast(receiving_items.amount as DECIMAL(12,4))) as ulazi,
+                       0 as prodaja,
+                       0 as otpisi,
+                       0 as poslije
+                   from 
+                       receivings
+                       left join receiving_items
+                   on 
+                       receivings.id = receiving_items.receiving_id 
+                   where 
+                       receivings.posted = 1  
+                       and receivings.document_date > ? and receivings.document_date < ?
+                   group by
+                       receiving_items.article_id
+                   ) 
+                   union all
+                   (select 
+                       product_items.article_id,
+                       0 as prije,
+                       0 as ulazi,
+                       sum(cast(sale_items.amount * product_items.amount as DECIMAL(12,4))) * -1 as prodaja,
+                       0 as otpisi,
+                       0 as poslije
+                   from 
+                   sales 
+                   left join sale_items on sales.id = sale_items.sale_id 
+                   left join products on products.id = sale_items.product_id
+                   left join product_items on products.id = product_items.product_id 
+                   where 
+                       sale_items.canceled_by is null and
+                       sales.date > ? and sales.date < ?
+                   group by
+                       product_items.article_id
+                   )
+                   union all
+                   (
+                   select 
+                       product_items.article_id,
+                       0 as prije,
+                       0 as ulazi,
+                       0 prodaja,
+                       sum(cast(sale_items.amount * product_items.amount as DECIMAL(12,4))) as otpisi,
+                       0 as poslije
+                   from 
+                   sale_items 
+                   left join orders on sale_items.order_id = orders.id
+                   left join products on products.id = sale_items.product_id
+                   left join product_items on products.id = product_items.product_id 
+                   where 
+                       orders.table_id = 46
+                       /*and sale_items.canceled_by is null*/
+                       and sale_items.date > ? and sale_items.date < ?
+                   group by
+                       product_items.article_id
+                       )
+               ) as xyz
+               LEFT JOIN articles on xyz.article_id = articles.id 
+               LEFT JOIN art_show_gr on articles.prikaz_group_id = art_show_gr.id 
+               where xyz.article_id IS NOT NULL
+               group by 
+                   xyz.article_id,
+                   articles.name, 
+                   art_show_gr.naziv;
+               
+                   `,[req.query.id2, req.query.id1, req.query.id2, req.query.datum2, req.query.datum1, req.query.datum2, req.query.datum1, req.query.datum2, req.query.datum1], 
+                   function(error, results){
+                           if(error) {
+                               return res.status(500).json({
+                                   title: 'An error has occured',
+                                   error : error
+                               });
+                           }			
+                           res.status(200).json({
+                               message: 'Success',
+                               obj: results
+                           });
+                   }
+               );
+                               
+           }
+            );
 
-    /*
-        select 
-	articles.id, 
-	articles.name, 
-    art_show_gr.naziv as grupa,
-	sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as prije,
-    sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as poslije,
-    sum(case when (receivings.document_date > ? and receivings.document_date < ? ) then  cast(receiving_items.amount as DECIMAL(12,4)) else 0 end) as ulazi
-    from articles
-    LEFT JOIN bot_inventura_detail on articles.id = bot_inventura_detail.article_id
-    LEFT JOIN art_show_gr on art_show_gr.id = articles.prikaz_group_id
-    LEFT JOIN bot_inventura_master on
-        bot_inventura_detail.location_id = bot_inventura_master.location_id AND
-        bot_inventura_detail.inventory_id = bot_inventura_master.inventory_id
-    left join receiving_items on articles.id = receiving_items.article_id
-    left join receivings on receiving_items.receiving_id = receivings.id  
-    WHERE 
-    receivings.posted = 1  and
-    bot_inventura_master.inventory_id >= ? - ?
-    group by 
-        articles.id, 
-        articles.name, 
-        art_show_gr.naziv 
-    */
-    kon.query(`
-    select 
-	article_id,
-	articles.name, 
-	art_show_gr.naziv as grupa,
-
-    sum(prije) as prije,
-    sum(ulazi) as ulazi,
-    sum(prodaja) as prodaja,
-    sum(otpisi) as otpisi,
-    sum(poslije) as poslije
-    from
-    (
-	(select 
-		articles.id as article_id, 
-		sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as prije,
-		0 as ulazi,
-		0 as prodaja,
-		0 as otpisi,
-		sum(case when bot_inventura_master.inventory_id = ? then kolicina else 0 end) as poslije
-		from 
-			articles
-		LEFT JOIN 
-			bot_inventura_detail on articles.id = bot_inventura_detail.article_id
-		LEFT JOIN 
-			bot_inventura_master on bot_inventura_detail.location_id = bot_inventura_master.location_id and
-			bot_inventura_detail.inventory_id = bot_inventura_master.inventory_id
-		where 
-			bot_inventura_master.inventory_id >= ?
-		group by
-			articles.id
-	)
-	union all
-	(select 
-		receiving_items.article_id,
-		0 as prije,
-		sum(cast(receiving_items.amount as DECIMAL(12,4))) as ulazi,
-		0 as prodaja,
-		0 as otpisi,
-		0 as poslije
-	from 
-		receivings
-		left join receiving_items
-	on 
-		receivings.id = receiving_items.receiving_id 
-	where 
-		receivings.posted = 1  
-		and receivings.document_date > ? and receivings.document_date < ?
-	group by
-    receiving_items.article_id
-    ) 
-    union all
-    (select 
-        product_items.article_id,
-        0 as prije,
-        0 as ulazi,
-        sum(cast(sale_items.amount * product_items.amount as DECIMAL(12,4))) * -1 as prodaja,
-        0 as otpisi,
-        0 as poslije
-    from 
-    sales 
-    left join sale_items on sales.id = sale_items.sale_id 
-    left join products on products.id = sale_items.product_id
-    left join product_items on products.id = product_items.product_id 
-    group by
-        product_items.article_id
-    )
-    ) as xyz
-    LEFT JOIN articles on xyz.article_id = articles.id 
-    LEFT JOIN art_show_gr on articles.prikaz_group_id = art_show_gr.id 
-    where xyz.article_id IS NOT NULL
-group by 
-xyz.article_id,
-articles.name, 
-art_show_gr.naziv;
-
-    `,[req.query.id2, req.query.id1, req.query.id2, req.query.datum2, req.query.datum1], 
-    function(error, results){
-            if(error) {
-                return res.status(500).json({
-                    title: 'An error has occured',
-                    error : error
-                });
-            }			
-            res.status(200).json({
-                message: 'Success',
-                obj: results
-            });
-    }
-); console.log(req.query);
+   }
+    );
+ console.log(req.query);
 });
 router.get('/inventorydates', function(req, res) { //jedinstveni Datumi inventure 
     kon.query('Select distinct snapshot_dttm, inventory_id from bot_inventura_master',
